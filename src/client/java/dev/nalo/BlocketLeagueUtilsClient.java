b7f2e1f507fa3699c4f3c0d7f011de4ec9d943c4
nalo_ <me@nalo.dev>
2025-09-02 22:38:56 +0200

    💄 Add toast notification

--- src/client/java/dev/nalo/BlocketLeagueUtilsClient.java
+++ src/client/java/dev/nalo/BlocketLeagueUtilsClient.java
@@ -2,6 +2,7 @@ package dev.nalo;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.Entity;
@@ -9,6 +10,7 @@ import net.minecraft.entity.Entity;
 import org.lwjgl.glfw.GLFW;
 
 import dev.nalo.events.BallCamEvent;
+import dev.nalo.events.HudRenderEvent;
 
 public class BlocketLeagueUtilsClient implements ClientModInitializer {
 
@@ -17,6 +19,7 @@ public class BlocketLeagueUtilsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		WorldRenderEvents.START.register(new BallCamEvent());
+		HudRenderCallback.EVENT.register(new HudRenderEvent());
 	}
 
 	public static KeyBinding ballCamKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
---
+++ src/client/java/dev/nalo/events/HudRenderEvent.java
@@ -0,0 +1,26 @@
+package dev.nalo.events;
+
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+import static dev.nalo.BlocketLeagueUtilsClient.ballCamKeyBind;
+import static dev.nalo.BlocketLeagueUtilsClient.ballEntity;
+
+public class HudRenderEvent implements HudRenderCallback {
+
+    @Override
+    public void onHudRender(DrawContext context, float tickDelta) {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        if (ballEntity == null)
+            return;
+
+        context.drawText(client.textRenderer, "⦿ Ball Cam", 12, 9, 0xFB5454, true);
+        context.drawText(client.textRenderer,
+                "Press [" + ballCamKeyBind.getBoundKeyLocalizedText().getString() + "] to toggle", 12, 19,
+                0xffffff, true);
+        context.drawGuiTexture(new Identifier("minecraft:toast/advancement"), 2, 2, 160, 32);
+    }
+
+}
