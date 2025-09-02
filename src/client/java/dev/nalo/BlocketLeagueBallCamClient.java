b3015edec5d87f8c08cba39e05b2e139a31cda69
nalo_ <me@nalo.dev>
2025-09-02 13:40:15 +0200

    🎉 Initial commit

---
+++ src/client/java/dev/nalo/BlocketLeagueBallCamClient.java
@@ -0,0 +1,11 @@
+package dev.nalo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlocketLeagueBallCamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as
+		// rendering.
+	}
+}
